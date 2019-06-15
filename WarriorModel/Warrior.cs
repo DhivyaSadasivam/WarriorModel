c624898f00c9af7ba4ce106950e5f65b68801082
Dhivya Sadasivam <dhivyas1222@gmail.com>
2019-06-15 22:54:34 +0530

    Committing WarriorModel

---
+++ WarriorModel/Warrior.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WarriorModel
+{
+    public class Warrior
+    {
+        public string Name { get; set; }
+        public string Weapon { get; set; }
+        public override string ToString()
+        {
+            return Name + " saving the world with " + Weapon;
+        }
+    }
+}
